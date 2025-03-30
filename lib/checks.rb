34384efabe805b2fbc56163ed3c9144855b647e5
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-30 10:13:37 -0400

    WIP: checks module

--- lib/checks.rb
+++ lib/checks.rb
@@ -1,45 +1,25 @@
 # frozen_string_literal: true
 
-def all_equal?(nested_arr)
-  nested_arr.each do |arr|
-    return true if arr.uniq.size <= 1
-  end
-end
-
-class Board
-  def initialize
-    @board_array = [
-      %w[1 2 3],
-      %w[4 5 6],
-      %w[7 8 9]
-    ]
-  end
-
-  # method to alter single value
-  def turn(char, square)
-    row = (square / 3.0).ceil - 1
-    col = (square % 3) - 1
-    @board_array[row][col] = char
-    @board_array
+# Methods to validate game during play
+module Checks
+  def self.win_conditions(array)
+    # Obtain diagonals
+    diag_fwd = [] # from sq 1 to 9
+    diag_bwd = [] # from sq 3 to 7
+    array.each_index do |i|
+      diag_fwd << arr[i][i]
+      diag_bwd << arr[-i][i]
+    end
+    # Return nested array of possible wins
+    array + # Rowwise
+      array.transpose + # Columnwise
+      [diag_fwd] + [diag_bwd] # Diagonals
   end
 
-  protected
-
-  attr_accessor :board_array
-end
-
-def checks(arr)
-  # Obtain diagonals
-  diag_fwd = [] # from sq 1 to 9
-  diag_bwd = [] # from sq 3 to 7
-  arr.each_index do |i|
-    diag_fwd << arr[i][i]
-    diag_bwd << arr[-i][i]
+  def self.winner?(array)
+    array.each do |elem|
+      return true if elem.uniq.size <= 1
+    end
+    false
   end
-
-  arr + # Rowwise checks
-    arr.transpose + # Column checks
-    [diag_fwd] + # Diagonal checks
-    [diag_bwd]
-  # true if all_equal?(check_arr)
 end
