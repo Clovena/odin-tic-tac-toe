d5988ec9b6cd2ed58204e077ec33d8dab1c5a7d5
ZR Zachary Ricker (2749) <zjricker@gmail.com>
2025-04-01 14:54:45 -0400

    Add safeguard for invalid square selection. Add declaration of victor.

--- lib/board.rb
+++ lib/board.rb
@@ -17,14 +17,22 @@ class Board
 
     "\n #{squares[0]} | #{squares[1]} | #{squares[2]}
 ---+---+---\n #{squares[3]} | #{squares[4]} | #{squares[5]}
----+---+---\n #{squares[6]} | #{squares[7]} | #{squares[8]} \n"
+---+---+---\n #{squares[6]} | #{squares[7]} | #{squares[8]} \n\n"
   end
 
   # method to write single value into board
-  def turn(char, square)
+  def turn(player)
+    print 'Pick a square: '
+    square = gets[0].to_i
+
+    until [1, 2, 3, 4, 5, 6, 7, 8, 9].include?(square)
+      print 'Invalid input. Pick a square: '
+      square = gets[0].to_i
+    end
+
     row = (square / 3.0).ceil - 1
     col = (square % 3) - 1
-    @board[row][col] = char
+    @board[row][col] = player
     puts self.format(@board)
     @board
   end
--- main.rb
+++ main.rb
@@ -1,15 +1,5 @@
 # frozen_string_literal: true
 
-### Pseudocode
-#
-#
-# Initialize game. Consider asking for size? Or just declare as @@CONSTANT
-# Store characters in a 3x3 matrix.
-# Ask for input from each player - use toggle to determine which player is up.
-# Chomp until single character remains, then overwrite player icon in that space.
-#
-# Need check that input is only 1 thru 9.
-
 require_relative 'lib/board'
 require_relative 'lib/players'
 require_relative 'lib/checks'
@@ -24,21 +14,23 @@ class TicTacToe
     @players = Players.new
   end
 
-  def take_turn
-    print 'Pick a square: '
-    gets[0].to_i
-  end
-
   def over?
     Check.winner?(Check.win_conditions(@gameboard.board))
+  end
+
+  def declare_winner(name)
+    puts "#{name} wins!"
   end
 end
 
 game = TicTacToe.new
 
 until game.over?
-  game.gameboard.turn(game.players.char1, game.take_turn)
+  game.gameboard.turn(game.players.char1)
+  last_turn = game.players.name1
   break if game.over?
 
-  game.gameboard.turn(game.players.char2, game.take_turn)
+  game.gameboard.turn(game.players.char2)
+  last_turn = game.players.name2
 end
+game.declare_winner(last_turn)
